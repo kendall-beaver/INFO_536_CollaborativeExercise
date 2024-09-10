e16fe9b1fde04d4ee90b16ffd72907a3bc2baf53
Antonio <antonio@datalyticsintel.com>
2024-09-10 15:00:34 -0700

    Attack code

--- analysis.R
+++ analysis.R
@@ -10,3 +10,18 @@ gtd_data <- read.csv("globalterrorismdb_0718dist.csv",
 
 # Basic data exploration
 glimpse(gtd_data)
+
+#How many attacks 
+attacks_per_year <- gtd_data %>%
+  count(iyear)
+
+# Plot the results
+ggplot(attacks_per_year, aes(x = iyear, y = n)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Number of Terrorist Attacks Per Year",
+       x = "Year", y = "Number of Attacks") +
+  theme_minimal()
+
+
+attack_type <- gtd_data %>%
+  attacktype1_txt
