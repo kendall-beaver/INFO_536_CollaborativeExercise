15864e35eb67f9f5e4f9c41487b61dbab6729914
VISHAL BHASHYAAM <vishalb@arizona.edu>
2024-09-11 12:49:06 -0700

    Updated Answer for Role 2 - Vishal Bhashyaam

--- analysis.R
+++ analysis.R
@@ -1,12 +1,21 @@
 # Global Terrorism Database Collaborative Analysis
-
+# Hello
 # Load necessary libraries
 library(dplyr)
 library(ggplot2)
 
 # Load the dataset
-gtd_data <- read.csv("globalterrorismdb_0718dist.csv", 
+gtd_data <- read.csv("/Users/vishalbhashyaam/Documents/GitHub/DS-public-interests/INFO_536_CollaborativeExercise/globalterrorismdb_0718dist.csv", 
                      stringsAsFactors = FALSE)
+
 
 # Basic data exploration
 glimpse(gtd_data)
+
+
+# Work: Group 3 - Vishal Bhashyaam
+# Role 2 - Count the attacks
+attack_type_count <- gtd_data %>% 
+  count(attacktype1_txt)
+
+print (attack_type_count)
