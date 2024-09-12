2946bc42eb43a3b32d4d23524c8e82b49f448481
vishal <vishalb@arizona.edu>
2024-09-12 14:59:50 -0700

    Vishal Code changes for role 2

--- analysis.R
+++ analysis.R
@@ -11,18 +11,6 @@ gtd_data <- read.csv("globalterrorismdb_0718dist.csv",
 # Basic data exploration
 glimpse(gtd_data)
 
-#How many attacks 
-attacks_per_year <- gtd_data %>%
-  count(iyear)
-
-# Plot the results
-ggplot(attacks_per_year, aes(x = iyear, y = n)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Number of Terrorist Attacks Per Year",
-       x = "Year", y = "Number of Attacks") +
-  theme_minimal()
-#test with data above 
-
 ##Role 1 : Antonio 
 #Group the data by attack type
 attack_type_counts <- gtd_data %>%#need the count of the types of attacks
@@ -33,3 +21,9 @@ attack_type_counts <- gtd_data %>%#need the count of the types of attacks
 print(attack_type_counts)
 
 
+# Work: Group 3 - Vishal Bhashyaam
+# Role 2 - Count the number of each attack type 
+attack_type_counts <- gtd_data %>% 
+  count(attacktype1_txt) # use count inbuilt function to get the count fo attack types
+
+print (attack_type_count) # printing attack types 
