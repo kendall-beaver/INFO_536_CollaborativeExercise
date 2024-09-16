c0c8a6e2c83e74577b554b9883eb4f22ec60cefd
kendall-beaver <kendallbeaver@arizona.edu>
2024-09-15 18:53:19 -0700

    cleaning up; not finished yet

--- analysis.R
+++ analysis.R
@@ -1,6 +1,11 @@
+#########################################################
+#
 # Global Terrorism Database Collaborative Analysis
-
-##### Load necessary libraries
+#
+# Group 3 Objective: Analyze the Most Common Attack Types
+#
+#########################################################
+# Load necessary libraries
 library(dplyr)
 library(ggplot2)
 
@@ -9,21 +14,26 @@ gtd_data <- read.csv("globalterrorismdb_0718dist.csv",
                      stringsAsFactors = FALSE)
 
 # Basic data exploration
-glimpse(gtd_data)
+glimpse(gtd_data) #135 columns
 
-##Role 1 : Antonio 
-#Group the data by attack type
-attack_type_counts <- gtd_data %>%#need the count of the types of attacks
+#########################################################
+# Role 1 : Antonio (Group the data by attack type.)
+attack_type_counts <- gtd_data %>% #need the count of the types of attacks
   group_by(attacktype1, attacktype1_txt)  #group by attack type number classification and description
-
+attack_type_counts
 
 # View the grouped and prepped data
 print(attack_type_counts)
 
+#########################################################
+# Role 2 Vishal Bhashyaam (Count the number of each attack type.)
 
-# Work: Group 3 - Vishal Bhashyaam
-# Role 2 - Count the number of each attack type 
 attack_type_counts <- gtd_data %>% 
   count(attacktype1_txt) # use count inbuilt function to get the count fo attack types
 
-print (attack_type_count) # printing attack types 
+print (attack_type_count) # printing attack types
+
+#########################################################
+# Role 3 : Kendall (Visualize the most common attack types using a bar chart.)
+
+
